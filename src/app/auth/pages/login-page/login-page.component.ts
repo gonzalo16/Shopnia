857e82eaba3b7697d69d21c3a75477e37695654e
gonzalo16 <marinucci.gonzalo@gmail.com>
2025-06-09 23:03:19 +0200

    Validando la autenticacion en el service Auth

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login-page',
@@ -10,9 +11,11 @@ import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 })
 export class LoginPageComponent {
   
-  formBuilder = inject(FormBuilder);
+   formBuilder = inject(FormBuilder);
+   hasErrorSignal = signal(false); 
+   authService = inject(AuthService);
+   enrutador = inject(Router);
 
-  hasErrorSignal = signal(false); 
   form = this.formBuilder.group({
     email:['',[Validators.required,Validators.email]],
     password:['',[Validators.required, Validators.minLength(6)]],
@@ -26,7 +29,15 @@ export class LoginPageComponent {
       },2000);
       return ;
     }
-
+    const {email = '',password = ''} = this.form.value;
     console.log(this.form.value);
+    this.authService.login(email!,password!).subscribe((isValid) => {
+      if(isValid){
+        this.enrutador.navigateByUrl('/');
+        return;
+      }
+
+      this.hasErrorSignal.set(true);
+    });
   }
 }
--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,14 +1,22 @@
 import { computed, inject, Injectable, signal } from '@angular/core';
 import { User } from '../interfaces/user.interface';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import { AuthResponse } from '../interfaces/auth-response.interface';
+import { catchError, map, Observable, of, tap } from 'rxjs';
 
 type AuthStatus = 'checking' | 'authenticated' | 'not-authenticated';
+const baseUrl = environment.baseUrl;
+
 
 @Injectable({
   providedIn: 'root'
 })
+
+
 export class AuthService {
 
+  
   private _authStatus = signal<AuthStatus>('checking');
   private _user = signal<User | null>(null);
   private _token = signal<string | null>(null);
@@ -35,5 +43,26 @@ export class AuthService {
   public getToken = computed<string | null>(() => {
     return this._token();
   })
+
+  public login(email:string, password:string): Observable<boolean>{
+    return this.httpClient.post<AuthResponse>(`${baseUrl}/auth/login`,{
+      email:email,
+      password:password
+    }).pipe(
+      tap(resp => {
+        this._user.set(resp.user);
+        this._authStatus.set('authenticated');
+        this._token.set(resp.token);
+        localStorage.setItem('token',resp.token);
+      }),
+      map(()=>true),
+      catchError((err:any) => {
+        this._user.set(null);
+        this._authStatus.set('not-authenticated');
+        this._token.set(null);
+        return of(false);
+      })
+    )
+  }
 
 }
